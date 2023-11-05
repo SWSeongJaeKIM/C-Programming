d9925c11e473350be06d53f0a856f6d058ea6244
SWSeongJaeKIM <118951938+SWSeongJaeKIM@users.noreply.github.com>
2023-11-06 00:34:51 +0900

    Add files via upload

---
+++ 2023-11-06(2)/c++_1.cpp
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int main()
+{
+    /*
+    std::cout << "Hi" << std::endl << "Hello" << std::endl;
+    */
+
+    //using std::cout;
+    //cout << "Hello" << std::endl;
+
+   /*
+    
+   cout << "Hello" << endl;
+   */
+    using namespace std;
+
+    int width, height;
+
+    cout << "너비와 높이 입력" << endl;
+    cin >> width >> height;
+
+
+    return 0;
+}
